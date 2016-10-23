0200e3e71346fcc06bf8e22386282919bd8047ee
Bartolomeu Rodrigues <pbartmr@gmail.com>
2016-10-24 00:53:05 +0100

    performance greatly improved in image loading

--- src/org/academiadecodigo/frogger/display/SpriteTypes.java
+++ src/org/academiadecodigo/frogger/display/SpriteTypes.java
@@ -9,12 +9,12 @@ import org.academiadecodigo.frogger.game.Game;
 //set paths to images
 public enum SpriteTypes {
     FIELD ("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/field.gif","C:\\Users\\gmkba\\Documents\\Frogger2.0\\src\\org\\academiadecodigo\\frogger\\display\\res\\field.gif"),
-    PLAYER("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/rafa.png","C:\\Users\\gmkba\\Documents\\Frogger2.0\\src\\org\\academiadecodigo\\frogger\\display\\res\\rafa.png"),
-    RAT("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/rato.png","C:\\Users\\gmkba\\Documents\\Frogger2.0\\src\\org\\academiadecodigo\\frogger\\display\\res\\rato.png"),
-    GRASS ("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/grass.png","C:\\Users\\gmkba\\Documents\\Frogger2.0\\src\\org\\academiadecodigo\\frogger\\display\\res\\grass.png"),
-    DUCK("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/duck.png",""),
-    PUFF  ("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/puff_circular.png",""),
-    MAC   ("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/mac.png","");
+    PLAYER("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/rafa.gif","C:\\Users\\gmkba\\Documents\\Frogger2.0\\src\\org\\academiadecodigo\\frogger\\display\\res\\rafa.gif"),
+    RAT("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/rato.gif","C:\\Users\\gmkba\\Documents\\Frogger2.0\\src\\org\\academiadecodigo\\frogger\\display\\res\\rato.gif"),
+    GRASS ("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/grass.gif","C:\\Users\\gmkba\\Documents\\Frogger2.0\\src\\org\\academiadecodigo\\frogger\\display\\res\\grass.gif"),
+    DUCK("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/duck.gif",""),
+    PUFF  ("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/puff_circular.gif",""),
+    MAC   ("/Users/codecadet/Project/Frogger2.0/src/org/academiadecodigo/frogger/display/res/mac.gif","");
 
     private String macPath;
     private String winPath;
